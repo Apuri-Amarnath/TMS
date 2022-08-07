8efe2ff0e1762a717449fbc81af511888a326d78
Apuri-Amarnath <apuriamarnath123@gmail.com>
2022-08-07 16:46:10 +0530

    Initial commit

---
+++ src/main/java/amar/beginner/examples/HelloWord.java
@@ -0,0 +1,15 @@
+package  amar.beginner.examples;
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+        System.out.println("Hello World asdfkjas;kfjlasjdfajsdf");
+    }
+}
